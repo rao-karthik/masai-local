5e6f9775625822cb7ef3adac37e01ee8e6515d12
Kartik Yadav <kartikyadav94@gmail.com>
2021-08-15 20:46:45 +0530

    DSA

--- Unit-6/Week-32/21/16_infixToPostfix.js
+++ Unit-6/Week-32/21/16_infixToPostfix.js
@@ -1,10 +1,43 @@
 function runProgram(input){
-    input = input.trim().split(/[\r\n]+/);
-    console.log(input)
+    let inp = input.trim(); 
+    // console.log(input);
+
+    let len = inp.length;
+    let output = "";
+    let stack = [];
+
+    for (let i = 0; i < len; i++){
+        if((inp[i] >= 'a' && input[i] <= 'z') || (inp[i] >= 'A' && inp[i] <= 'Z') || (inp[i] >= 0 && inp[i] <= 9)){
+            output += inp[i];
+        }
+        else if(inp[i] === '('){
+            stack.push('(');
+        }
+        else if(inp[i] === ')'){
+            while(stack[stack.length-1] !== "("){
+                output += stack[stack.length-1]
+                stack.pop();
+            }
+            stack.pop();
+        }
+        else {
+            let preced = precedence(inp[i]);
+            while(stack.length > 0 && preced <= precedence(stack[stack.length-1])){
+                output += stack[stack.length-1];
+                stack.pop();
+            }
+            stack.push(inp[i]);
+        }
+    }
+    while(stack.length > 0){
+        output += stack[stack.length-1];
+        stack.pop();
+    }
+    console.log(output);
 }
 
 if (process.env.USER === "kartik") {
-    runProgram(``);
+    runProgram(`a+b-c+d*(e-f)/g+(h*(i/j))`);
 }
 else {
     process.stdin.resume();
@@ -23,4 +56,16 @@ else {
         runProgram(read);
         process.exit(0);
     });
+}
+
+function precedence(char){
+    if(char === '^'){
+        return 3;
+    }
+    else if(char === '*' || char === '/'){
+        return 2;
+    }
+    else if(char === '+' || char === '-'){
+        return 1;
+    }
 }
